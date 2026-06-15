5a3bc120b3fb201e5003ae53e456d571cb4abe19
muuhacker-oss <muuhacker@gmail.com>
2026-06-15 23:20:26 +0300

    Add settings configuration for CYBERMUU bot

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'CYBERMUU,
+  author: 'ETHICALMUU,
+  botName: "CYBERMUU",
+  botOwner: 'ETHICALMUU, // Your name
+  ownerNumber: '255625816690', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.2",
+  updateZipUrl: "https://github.com/muuhacker-oss/ETHICALMUU_BOT.git/tree/main.Zip",
+};
+
+module.exports = settings;
